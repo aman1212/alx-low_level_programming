74607ce6d5bab5a34768a79e37c414247132b7bc
aman <amanthebest132@gmail.com>
2022-03-11 16:42:48 +0300

    Commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   int n=rand();
+
+   if(n >0)
+     {
+	printf("is positive");
+     }
+
+   else
+    {
+  	if(n<0)
+	{
+	 printf("is negative");
+	}
+	else
+	{
+	 printf("is zero")
+	}
+    }
+   return 0;
+}
