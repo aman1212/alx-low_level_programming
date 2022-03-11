7f8682bd8d0898401347e4414251e16cdaa2509f
aman <amanthebest132@gmail.com>
2022-03-11 16:57:51 +0300

    Commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 int main() {
-   // printf() displays the string inside quotation
-   int n=rand();
+// printf() displays the string inside quotation
+int n=rand();
 
-   if(n >0)
-     {
-	printf("is positive");
-     }
-
-   else
-    {
-  	if(n<0)
-	{
-	 printf("is negative");
-	}
-	else
-	{
-	 printf("is zero")
-	}
-    }
-   return 0;
+if(n >0)
+{
+printf("is positive");
+}
+else
+{
+if(n<0)
+{
+printf("is negative");
+}
+else
+{
+printf("is zero");
+}
+}
+return 0;
 }
