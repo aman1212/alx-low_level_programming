c8844e0c1ffddc20e842bf03d8a34441e3abce76
aman <amanthebest132@gmail.com>
2022-03-11 18:09:58 +0300

    Commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ printf("Last digit of %d is %d and is less than 6 and not 0",n,lastDigit);
 }
 if(lastDigit>5)
 {
-printf("Last digit of %d is %d and is greater than 5",n,lastDigit);
+printf("Last digit of %d is $d and is greater than 5",n,lastDigit);
 }
 return (0);
 }
