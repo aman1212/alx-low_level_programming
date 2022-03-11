59268f96392153646f835509c8f789215473d2bc
aman <amanthebest132@gmail.com>
2022-03-11 18:05:48 +0300

    Commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,9 +9,10 @@
 int main(void)
 {
 int n;
+int lastDigit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-int lastDigit = n % 10;
+lastDigit = n % 10;
 /* your code goes there */
 if(lastDigit==0)
 {
