1033d7f70e826592c020f2e1799045c24a4aaf3e
aman <amanthebest132@gmail.com>
2022-03-15 21:53:55 +0300

    Commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,16 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-printf("_putchar\n");
-return (0);
+	char word[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+
+	return (0);
 }
