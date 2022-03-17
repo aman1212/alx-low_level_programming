b8bb562b4d4897a2a0c6d13509b3898617a588b3
aman <amanthebest132@gmail.com>
2022-03-17 18:41:29 +0300

    new commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,43 +1,21 @@
-#include "main.h"
 #include <stdio.h>
-/**
- * main - check the code 
- *
- * Return: void.
- */
-
+ 
 int main(void)
 {
-
-	int i = 1;
-
-	while (i <= 100)
-	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else
-		{
-			printf("%i", i);
-		}
-
-		if (i <= 100)
-		{
-			putchar(' ');
-		}
-
-		i++;
-	}
-	putchar('\n');
-	return (0);
-
+    int i;
+    for (i=1; i<=100; i++)
+    {
+        
+        if (i%15 == 0)       
+            printf ("FizzBuzz\t");   
+        else if ((i%3) == 0)   
+            printf("Fizz\t");                
+        else if ((i%5) == 0)                      
+            printf("Buzz\t");                
+  						else         
+            printf("%d\t", i);                
+ 
+    }
+ 
+    return 0;
 }
