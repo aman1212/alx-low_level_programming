2490c2ee264c9c592c9a995753f53c198ce57c37
aman <amanthebest132@gmail.com>
2022-03-17 16:36:46 +0300

    Commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ *_islower - checks for lowercase characters
+ *
+ *Description: checks for lowercase
+ *
+ *Return:  1 - l0wercase 0 - otherwise
+ *
+ *@c:input
+ */
+int _islower(int c)
+{
+	if(isupper(ch))
+    {
+         return (1);
+    }
+    else if(islower(ch))
+    {
+        return (0);
+    }
+    else
+    {
+        return (0);
+    }
+}
