0ead821cce90213720036d5d685387cbfb55fe4b
aman <amanthebest132@gmail.com>
2022-03-21 18:18:58 +0300

    new commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,12 +13,7 @@ void print_array(int *a, int n)
 int i;
 for (i = 0; i < n ; i++)
 {
-printf("%d", *(a + i));
-if (i != n - 1)
-{
-_putchar(',');
-_putchar(' ');
-}
+printf("%d, ", *(a + i));
 }
 _putchar('\n');
 }
