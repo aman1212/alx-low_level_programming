f656765fbcecd7e672bad3c2df78145e859ca0e2
aman <amanthebest132@gmail.com>
2022-03-21 17:20:28 +0300

    new commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str : to be printed
+ * Return:void
+ */
+
+void puts2(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+str++;
+}
+_putchar('\n');
+}
