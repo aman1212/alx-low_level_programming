6eda87b74670aa00920362678ac236d5f7f1c54a
aman <amanthebest132@gmail.com>
2022-03-21 15:10:36 +0300

    new commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * rameter and updates the value it points to to 98
+ * @c : character to check
+ * Return:0 or 1
+ */
+
+void reset_to_98(int *n)
+{
+ *n = 98;
+ return;
+}
