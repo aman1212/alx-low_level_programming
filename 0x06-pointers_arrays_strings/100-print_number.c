d4c84828393b9111ee0956c861c4942b371e98fc
aman <amanthebest132@gmail.com>
2022-03-24 16:04:36 +0300

    commited

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
 * print_number - print numbers chars
 * @n: integer
@@ -6,16 +6,12 @@
 **/
 void print_number(int n)
 {
-unsigned int n1;
-n1 = n;
-if (n < 0)
+if (n < 0) 
 {
 _putchar('-');
-n1 = -n;
+n = -n;
 }
-if (n1 / 10 != 0)
-{
-print_number(n1 / 10);
-}
-_putchar((n1 % 10) + '0');
+if (n/10)
+ print_number(n/10);
+_putchar(n%10 + '0');
 }
