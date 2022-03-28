e3f255aa9404ccf70b43edeffd2e680609ca1b0c
aman <amanthebest132@gmail.com>
2022-03-28 17:45:56 +0300

    new commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,10 +11,9 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned char *p = s;
-while (n--)
-{
-*p++ = (unsigned char)b;
-}
-return s;
+unsigned int index;
+char *memory = s, value = b;
+for (index = 0; index < n; index++)
+memory[index] = value;
+return (memory);
 }
