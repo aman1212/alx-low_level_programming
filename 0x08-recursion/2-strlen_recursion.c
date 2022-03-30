33885d9bb2b0b89d876b794aae23d87a40f8749f
aman <amanthebest132@gmail.com>
2022-03-30 19:01:25 +0300

    new commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,17 +3,17 @@
 /**
 * _strlen_recursion -returns the length of a string
 *@s: string
-*Return: String lenght 
+*Return: String lenght
 */
 
 int _strlen_recursion(char *s)
 {
 if (!*s)
 {
-return 0;
+return (0);
 }
 else
 {
-return 1 + _strlen_recursion(s + 1);
+return (1 + _strlen_recursion(s + 1));
 }
 }
