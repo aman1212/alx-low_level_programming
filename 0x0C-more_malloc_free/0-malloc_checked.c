ab70101f03f4f88e744154a4d762dceed141f5f9
aman <amanthebest132@gmail>
2022-04-07 14:17:22 +0300

    commited

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *malloc_checked - allocate memory
+ *
+ *@b:input
+ *
+ *Description:prints sign
+ *
+ *Return: address
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+    char *x;
+    
+    x = malloc(b);
+    
+    return x;
+     
+}
