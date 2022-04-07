836e21427c17ed52616b4176585ac3ff537d2c96
aman <amanthebest132@gmail>
2022-04-07 14:52:44 +0300

    commited

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,36 +1,57 @@
-#include <main.h>
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
+
 /**
-* _realloc - multiplies two numbers
-* @ptr: pointer
-* @old_size: old size
-* @new_size: new size
-*Return: pointer
-*/
+ * _memcpy - copy memory data from src to dest
+ * @dest: memory destination
+ * @src: memory source
+ * @n: size of new memory
+ */
+void _memcpy(void *dest, void *src, size_t n)
+{
+	size_t i;
+	char *csrc = (char *)src;
+	char *cdest = (char *)dest;
+
+	for (i = 0; i < n; i++)
+		cdest[i] = csrc[i];
+}
+
+/**
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: array length
+ * @old_size: size of old memory
+ * @new_size: size of new memory
+ * Return: pointer to new memory
+ */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-unsigned int i;
-int *new;
-if (new_size == old_size)
-return (ptr);
-if (ptr == NULL)
+
+void *newPtr;
+if (new_size == 0)
 {
-new = malloc(new_size);
-if (new == NULL)
-return (NULL);
-return (new);
+	if (ptr != NULL)
+		free(ptr);
+	return (NULL);
 }
-if (new_size == 0 && ptr != NULL)
+else if (!ptr)
 {
-free(ptr);
-return (NULL);
+	return (malloc(new_size));
 }
-new = malloc(new_size);
-if (new == NULL)
-return (NULL);
-for (i = 0; i < old_size && i < new_size; i++)
-new[i] = ((char *)ptr)[i];
-free(ptr);
-return (new);
+else if (new_size <= old_size)
+{
+	return (ptr);
+}
+else
+{
+	newPtr = malloc(new_size);
+	if (newPtr)
+	{
+		_memcpy(newPtr, ptr, old_size);
+		free(ptr);
+	}
+	return (newPtr);
+}
+
+return (0);
 }
--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,56 +1,91 @@
-#include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include "holberton.h"
+
 /**
- * _atoi - Converts a character array to an integer
- * @s: character array to convert
- * Return: returns integer from array, returns 0 if none found
- */
-int _atoi(char *s)
+ * _puts - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
+*/
+
+
+void _puts(char *str)
 {
-int retval, i, neg;
-i = 0;
-retval = 0;
-neg = -1;
-while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+int i = 0;
+while (str[i])
 {
-if (s[i] == '-')
-neg *= -1;
-i++;
+	_putchar(str[i]);
+	i++;
 }
-while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
-retval = (retval * 10) - (s[i++] - '0');
-return (retval *neg);
+
 }
+
 /**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
+ * _atoi - convert a string to an integer.
+ * @s: char type string
+ * Return: integer converted
  */
-int main(int argc, char *argv[])
+
+int _atoi(const char *s)
 {
-int a, b, i, j;
-unsigned long mul;
+    int sign = 1;
+	unsigned long int resp = 0, firstNum, i;
+
+	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	{
+		if (s[firstNum] == '-')
+		{
+			sign *= -1;
+		}
+	}
+
+	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		resp *= 10;
+		resp += (s[i] - 48);
+	}
+
+	return (sign * resp);
+}
+
+/**
+ * print_int - prints an integer.
+ * @n: int
+ * Return: 0
+ */
+
+void print_int(unsigned long int n)
+{
+
+unsigned  long int divisor = 1, i, resp;
+
+for (i = 0; n / divisor > 9; i++, divisor *= 10)
+;
+
+for (; divisor >= 1; n %= divisor, divisor /= 10)
+{
+	resp = n / divisor;
+	_putchar('0' + resp);
+}
+
+}
+
+/**
+ * main - print the result of the multiplication, followed by a new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+(void)argc;
+
 if (argc != 3)
 {
-printf("Error\n");
-exit(98);
+	_puts("Error ");
+	exit(98);
 }
-for (i = 1; i < argc; i++)
-{
-for (j = 0; argv[i][j] != '\0'; j++)
-{
-if (argv[i][j] > 57 || argv[i][j] < 48)
-{
-printf("Error\n");
-exit(98);
-}
-}
-}
-a = _atoi(argv[1]);
-b = _atoi(argv[2]);
-mul = a *b;
-printf("%lu\n", mul);
+print_int(_atoi(argv[1]) * _atoi(argv[2]));
+_putchar('\n');
+
 return (0);
 }
