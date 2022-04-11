eeaa5b737b41cb30cb293668abf4a80565291693
aman <amanthebest132@gmail.com>
2022-04-11 17:59:16 +0300

    new commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,15 @@
-#ifndef DOG
-#define DOG
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - struct name, owner, age
+* @name: name
+* @age: age
+* @owner: owner
+*/
 struct dog
 {
- char *name;
- float age;
- char *owner;
+char *name;
+float age;
+char *owner;
 };
 #endif
